3e4eaa6449cc32731afeaf878094115c7ac600ee
René Hernández <renehr9102@gmail.com>
2015-04-28 12:12:00 -0700

    Rename CommandImporter to CommandManager
    Move ImportAllCommands to manager constructor
    Add 1 test

--- src/tests/NetConsole.Core.Tests/CommandImporterTest.cs
+++ src/tests/NetConsole.Core.Tests/CommandImporterTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using NetConsole.Core.Attributes;
+using NetConsole.Core.Interfaces;
 using NetConsole.Core.Managers;
 using NUnit.Framework;
 
@@ -12,19 +14,32 @@ namespace NetConsole.Core.Tests
     public class CommandImporterTest
     {
 
-        private CommandImporter _importer;
+        private CommandManager _manager;
+
+        [NotRegistrable]
+        private class NotRegistrableCommand : ICommand
+        {
+            public int Status { get; private set; }
+            public string Name { get; private set; }
+            public string Overview { get; private set; }
+        }
 
         [SetUp]
         public void SetUp()
         {
-            _importer = new CommandImporter();
-            _importer.ImportAllCommands();
+            _manager = new CommandManager();
+        }
+
+        [Test]
+        public void Test_CommandManagerNotImportNotRegistrable()
+        {
+            Assert.AreEqual(3, _manager.Factory.GetAll().Count());
         }
 
         [Test]
         public void Test_GetOutputFromString()
         {
-            var output = _importer.GetOutputFromString("echo:echoed unique trial");
+            var output = _manager.GetOutputFromString("echo:echoed unique trial");
 
             Assert.AreEqual(1, output.Length);
             Assert.AreEqual(0, output[0].Status);
